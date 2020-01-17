8fb20a9679867c8d17a80cc8fc479eb0f3bdb977
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2020-01-17 00:13:01 -0800

    1_3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,60 +5,17 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Product> products = new ArrayList<>();
-        Product versache = new Product("Versache");
-        versache.productPrice();
-        versache.productRating();
-        Product armani = new Product("Armani");
-        armani.productPrice();
-        armani.productRating();
-        Product zara = new Product("Zara");
-        zara.productPrice();
-        zara.productRating();
-        Product celio = new Product("Celio");
-        celio.productPrice();
-        celio.productRating();
-        Product smog = new Product("Smog");
-        smog.productPrice();
-        smog.productRating();
-        Product maxAndMara = new Product("Max & Mara");
-        maxAndMara.productPrice();
-        maxAndMara.productRating();
-        Product luiVuiton = new Product("Lui Vuiton");
-        luiVuiton.productPrice();
-        luiVuiton.productRating();
-        Product guchi = new Product("Guchi");
-        guchi.productPrice();
-        guchi.productRating();
-        Product sali = new Product("Sali");
-        sali.productPrice();
-        sali.productRating();
-        Product sarojani = new Product("Sarojani");
-        sarojani.productPrice();
-        sarojani.productRating();
+        Author author1 = new Author("Loran"," Dishdishyan ", Author.Gender.MALE);
+        Author author2 = new Author("Moris"," Berberyan ", Author.Gender.OTHER);
+        Author author3 = new Author("Naira"," Mkrtchyan ", Author.Gender.FEMALE);
 
-        products.add(versache);
-        products.add(armani);
-        products.add(zara);
-        products.add(celio);
-        products.add(smog);
-        products.add(maxAndMara);
-        products.add(luiVuiton);
-        products.add(guchi);
-        products.add(sali);
-        products.add(sarojani);
+        Book book = new Book("Anurjner",new ArrayList<>(),22.5);
+        book.getAuthors().add(author1);
+        book.getAuthors().add(author2);
+        book.getAuthors().add(author3);
 
-        int bestRatedProduct = 0;
-        for (int i = 0; i < products.size()-1; i++) {
-            if (products.get(i).productRating() > products.get(i + 1).productRating()){
-                bestRatedProduct = products.get(i).productRating();
-            }
-
-        }
-        for (int i = 0; i < products.size()-1; i++) {
-            if (bestRatedProduct == products.get(i).productRating()){
-                System.out.println(products.get(i).getTitle());
-            }
+        for (int i = 0; i < book.getAuthors().size(); i++) {
+            System.out.println(book.getAuthors().get(i).getName() + book.getAuthors().get(i).getSurname() + book.getAuthors().get(i).getGender());
         }
     }
 }
